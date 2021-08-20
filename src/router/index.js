366e2b0b2d72b69bbbc9ce47603e19cf981c491b
DarkoiV <makkob997@gmail.com>
2021-08-20 12:33:38 +0200

    Added not found page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Board from '../views/Board.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -7,6 +8,11 @@ const routes = [
     name: 'Home',
     component: Board,
     props: true
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: 'NotFound',
+    component: NotFound,
   },
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,35 @@
+<template>
+  <p id="fourohfour"> 404 </p>
+  <p id="notfound">Not Found </p>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+}
+</script>
+
+<style scoped>
+#fourohfour {
+  margin: 30px;
+  text-align: center;
+  color: salmon;
+
+  font: Monaco, monospace;
+  font-size: 40px;
+  font-weight: 600;
+
+  user-select: none;
+}
+#notfound {
+  margin: 30px;
+  text-align: center;
+  color: salmon;
+
+  font: Monaco, monospace;
+  font-size: 24px;
+  font-weight: 600;
+
+  user-select: none;
+}
+</style>
