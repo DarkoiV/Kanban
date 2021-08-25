c161ed1838fd97cc821fb18544d885228d753c2c
DarkoiV <makkob997@gmail.com>
2021-08-25 11:41:01 +0200

    Added create script

---
+++ create.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Create build folder
+test -d "build" || mkdir build
+
+# Create new folder for current build
+FOLDER_NAME=$(date | awk '{print $2 "-" $3 "T" $5}' | sed -r 's/:/./g')
+mkdir build/$FOLDER_NAME
+
+# Build go binary
+cd backend
+go build -o ../build/$FOLDER_NAME/server
+cd ..
+
+# Build Vue SPA
+cd frontend
+npm run build
+test -d dist && mv dist ../build/$FOLDER_NAME
+cd ..
