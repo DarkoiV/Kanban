fdb2b185a0dec5b86ff104e0ee7cdf8e8db21588
DarkoiV <makkob997@gmail.com>
2021-08-27 12:59:20 +0200

    Updated setupenv script

--- setupenv.sh
+++ setupenv.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # First argument is target directory
-if ! test $1 = ; then
-	mkdir -p $1 && cd $1
+if ! test "$1" = ; then
+	mkdir -p "$1" && cd "$1"
 fi
 
 echo Creating new .env
@@ -11,6 +11,16 @@ echo Creating new .env
 touch .env
 echo '# ENV used by kanban app' > .env
 
+echo
+read -p 'Application port[9000]: ' APP_PORT
+echo "APP_PORT=${APP_PORT:-9000}" >> .env
+
+read -p 'Admin user pswd[admin]: ' APP_PASS
+echo "APP_PASS=${APP_PASS:-admin}" >> .env
+
+echo '\n# Database' >> .env
+
+echo
 read -p 'Database name[kanbanDB]: ' DB_NAME
 echo "DB_NAME=${DB_NAME:-kanbanDB}" >> .env
 
@@ -27,8 +37,8 @@ read -p 'Database pswd[kanban4321]: ' DB_PASS
 echo "DB_PASS=${DB_PASS:-kanban4321}" >> .env
 
 echo
-echo ----- FILE -- .env -- FILE -----
+echo "----- FILE -- .env -- FILE -----"
 cat .env
-echo ----- FILE -- .env -- FILE -----
+echo "----- FILE -- .env -- FILE -----"
 echo
 echo Make sure env are correct!
